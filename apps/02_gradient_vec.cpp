2ff417fa107a37facd4e4287377903b590daadf9
shamanskiy <shamanskiy.alexander@gmail.com>
2021-05-08 16:30:55 +0100

    use Vec3 in 02_gradient_vec.cpp

--- apps/02_gradient_vec.cpp
+++ apps/02_gradient_vec.cpp
@@ -9,9 +9,6 @@ int main() {
 
     int nx = 200;
     int ny = 100;
-    Ray ray;
-    Vec3 vec;
-    cout << vec << "\n";
 
     ofstream imageFile;
     imageFile.open("02_gradient_vec.ppm");
@@ -21,13 +18,11 @@ int main() {
     for (int i = 0; i < ny; i++)
         for (int j = 0; j < nx; j++)
         {
-            float r = float(i) / ny;
-            float g = float(j) / nx;
-            float b = 0.2;
+            Vec3 color(float(i) / ny, float(j) / nx, 0.2);
 
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            int ir = int(255.99 * color.r());
+            int ig = int(255.99 * color.g());
+            int ib = int(255.99 * color.b());
 
             imageFile << ir << " " << ig << " " << ib << "\n";
         }
