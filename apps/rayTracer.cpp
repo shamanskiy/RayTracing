cbd474c6aad494e93691a58fbcf9d4c0c170823b
shamanskiy <shamanskiy.alexander@gmail.com>
2021-05-09 17:28:40 +0100

    visualize sphere normals

--- apps/rayTracer.cpp
+++ apps/rayTracer.cpp
@@ -12,21 +12,30 @@ float map_y_to_zero_one(const Vec3& vec)
     return 0.5 * (unitDirection.y() + 1.0);
 }
 
-bool hit_sphere(const Vec3& center, float radius, const Ray& ray)
+float hit_sphere(const Vec3& center, float radius, const Ray& ray)
 {
     Vec3 AB = ray.origin() - center;
     float a = ray.direction().length_sq();
     float b = 2.0 * dot(ray.direction(), AB);
     float c = (ray.origin() - center).length_sq() - radius*radius;
-    return (b * b - 4 * a * c > 0);
+    float discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+        return -1;
+    else
+        return (- b - sqrt(discriminant)) / (2.0*a);
 }
 
 Vec3 color(const Ray& ray)
 {
-    if (hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, ray))
-        return Vec3(1.0, 0.0, 0.0);
+    float t = hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, ray);
+    if (t > 0.0)
+    {
+        Vec3 normal = (ray.eval(t) - Vec3(0.0, 0.0, -1.0)).normalize();
+        return 0.5* ( normal + Vec3(1.0,1.0,1.0));
+    }
+        
 
-    float t = map_y_to_zero_one(ray.direction());
+    t = map_y_to_zero_one(ray.direction());
     Vec3 white(1.0, 1.0, 1.0);
     Vec3 lightBlue(0.5, 0.7, 1.0);
     return lerp(white, lightBlue, t);
@@ -38,7 +47,7 @@ int main() {
     int ny = 100;
 
     ofstream imageFile;
-    imageFile.open("first_sphere.ppm");
+    imageFile.open("normal_sphere.ppm");
 
     imageFile << "P3\n" << nx << " " << ny << "\n255\n";
 
