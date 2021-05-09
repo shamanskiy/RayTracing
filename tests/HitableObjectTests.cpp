c3ab6c00af47345aa21f030075397571bbd78ff6
shamanskiy <shamanskiy.alexander@gmail.com>
2021-05-10 00:03:26 +0100

    change sphere radius in tests

--- tests/HitableObjectTests.cpp
+++ tests/HitableObjectTests.cpp
@@ -6,22 +6,22 @@
 SCENARIO("Sphere hit tests", "[Sphere]")
 {
     GIVEN("") {
-        Sphere sphere(Vec3(0.0, 0.0, 0.0), 1.0);
+        Sphere sphere(Vec3(0.0, 0.0, 0.0), 2.0);
 
         WHEN("") {
-            Ray ray(Vec3(2.0, 0.0, 0.0), Vec3(-1.0, 0.0, 0.0));
+            Ray ray(Vec3(5.0, 0.0, 0.0), Vec3(-1.0, 0.0, 0.0));
             auto hit = sphere.testHit(ray);
 
             THEN("got a hit") {
-                REQUIRE(hit.t == 1.0_a);
-                REQUIRE(hit.point == Vec3(1.0, 0.0, 0.0));
+                REQUIRE(hit.t == 3.0_a);
+                REQUIRE(hit.point == Vec3(2.0, 0.0, 0.0));
                 REQUIRE(hit.normal == Vec3(1.0, 0.0, 0.0));
             }
         }
 
         WHEN("") {
-            Ray ray(Vec3(2.0, 0.0, 0.0), Vec3(-1.0, 0.0, 0.0));
-            Interval interval(0.1, 0.5);
+            Ray ray(Vec3(5.0, 0.0, 0.0), Vec3(-1.0, 0.0, 0.0));
+            Interval interval(1.0, 2.0);
             auto hit = sphere.testHit(ray, interval);
 
             THEN("hit outside of interval")
@@ -29,7 +29,7 @@ SCENARIO("Sphere hit tests", "[Sphere]")
         }
 
         WHEN("") {
-            Ray ray(Vec3(2.0, 0.0, 0.0), Vec3(0.0, 1.0, 0.0));
+            Ray ray(Vec3(5.0, 0.0, 0.0), Vec3(0.0, 1.0, 0.0));
             auto hit = sphere.testHit(ray);
 
             THEN("ray misses the sphere")
@@ -37,12 +37,12 @@ SCENARIO("Sphere hit tests", "[Sphere]")
         }
 
         WHEN("") {
-            Ray ray(Vec3(2.0, 1.0, 0.0), Vec3(-1.0, 0.0, 0.0));
+            Ray ray(Vec3(5.0, 2.0, 0.0), Vec3(-1.0, 0.0, 0.0));
             auto hit = sphere.testHit(ray);
 
             THEN("ray touches the sphere"){
-                REQUIRE(hit.t == 2.0_a);
-                REQUIRE(hit.point == Vec3(0.0, 1.0, 0.0));
+                REQUIRE(hit.t == 5.0_a);
+                REQUIRE(hit.point == Vec3(0.0, 2.0, 0.0));
                 REQUIRE(hit.normal == Vec3(0.0, 1.0, 0.0));
             }
         }
